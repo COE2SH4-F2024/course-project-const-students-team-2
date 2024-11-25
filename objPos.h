e13614d5fa63d8f84cfaa31e5f8a371c45582f66
pathaa24 <pathaa24@mcmaster.ca>
2024-11-25 17:42:54 -0500

    changes

--- objPos.h
+++ objPos.h
@@ -15,11 +15,12 @@ class objPos
         char symbol;
 
         objPos();
-        objPos(int xPos, int yPos, char sym);
+        objPos(int xPos, int yPos, char sym); //other constructor 
         
         // Respect the rule of six / minimum four
         // [TODO] Implement the missing special member functions to meet the minimum four rule
         
+
         void setObjPos(objPos o);        
         void setObjPos(int xPos, int yPos, char sym);  
 
